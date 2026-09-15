3d3b8dd25b6201a5bf857803c85da61443c2752e
Engelard <98026248+Engelard@users.noreply.github.com>
2026-09-15 23:10:30 +0300

    Enemies + Knight completed

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -3,23 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 100
 const JUMP_VELOCITY = -250.0
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		anim_sprite.play("Jump")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("JUMP") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("MOVE LEFT", "MOVE RIGHT")
 	if direction:
 		velocity.x = direction * SPEED
+		anim_sprite.play("Running")
+		
+		if direction > 0:
+			anim_sprite.flip_h = false
+		else:
+			anim_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim_sprite.play("IDLE")
 
 	move_and_slide()
